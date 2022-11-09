8a4d351c234e15e8b140be60d6f5366a000b4be1
FozleRabbi5 <fozlerabbi190@gmail.com>
2022-11-09 16:26:34 +0600

    added data list

--- product/views/product.py
+++ product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
-
-from product.models import Variant
+from django.views.generic import ListView
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -12,3 +12,9 @@ class CreateProductView(generic.TemplateView):
         context['product'] = True
         context['variants'] = list(variants.all())
         return context
+
+
+class ProductList(ListView):
+    model = Product
+    context_object_name = 'product_list'
+    template_name = 'products/list.html'
